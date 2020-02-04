981ce21a29f324b87b9c2a068402229968f027f4
L Borje <lorenzo.borje@gmail.com>
2020-02-04 14:15:48 +0800

    completed work

---
+++ src/components/ChatShow.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="chat">
+    <div v-for="message in messages" :key="message.time">
+        <div v-if="message.host" class="host message">
+            <p>{{message.text}}</p>
+        </div>
+        <div v-else class="guest message">
+            <p>{{message.text}}</p>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ChatShow',
+  props: {
+      messages: Array 
+  }
+}
+</script>
+
+<style scoped>
+
+.chat {
+    display: flex;
+    flex-direction: column;
+    width: 90%;
+    margin: 0 auto;
+}
+
+.message {
+    border-radius: 5px;
+    margin: 10px;
+    padding: 10px;
+    width: 60%;
+}
+
+.host {
+    background-color: #000;
+    color: #FFF;
+    float: right;
+}
+
+.guest {
+    background-color: cornsilk;
+    float: left;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FullPage :options="options" id="fullpage">
+      <div class="section">
+        <div class="slide">
+          <h1>mixtape 000</h1>
+          <h2>on the issue of Hong Kong</h2>
+        </div>
+        <div v-for="(transcript, index) in transcripts" class="slide" :key="index">
+          <h1>{{transcript.title}}</h1>
+          <ChatShow :messages="transcript.messages "/>
+        </div>
+      </div>
+    </FullPage>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ChatShow from '@/components/ChatShow.vue'
+import FullPage from 'vue-fullpage.js/src/FullPage.vue'; 
+
+import transcript1 from '@/assets/transcriptA.json'
+import transcript2 from '@/assets/transcriptH.json'
+import transcript3 from '@/assets/transcriptC.json' 
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    ChatShow,
+    FullPage
+  },
+  data() {
+    return {
+      options: {
+        licenseKey: '8857DA6E-01F3472C-8AD8BF1A-CA320A28',
+        scrollOverflow: true,
+        slidesNavigation: true,
+        controlArrows: false,
+      },
+      transcripts: [
+        transcript1,
+        transcript2,
+        transcript3
+      ]
+    }
   }
 }
 </script>
